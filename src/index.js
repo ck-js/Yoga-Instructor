2ce44320c8f81c1fd0f076f847a6eaac518ba438
ck-js <ckentertainmentinfo@gmail.com>
2024-03-12 16:55:30 +0700

    initial git commit with logo, nav header and section 1 elements with background image with a flipchart h1 title effect that loops through array of strings to display who / what the client is

---
+++ src/index.js
@@ -0,0 +1,82 @@
+// import function components from page-load.js
+import {instructorName, yogaPoseImage, restaurantDescription, pageLoadNav, flipChartTitle } from "./page-load";
+// import css style file
+import './style.css'
+
+// get body element
+const body = document.body;
+
+// access content output container
+const contentOutput = document.getElementById('content')
+
+// append restaurant name to content output
+body.insertBefore(instructorName(), contentOutput)
+
+// append nav to content output
+body.insertBefore(pageLoadNav(), contentOutput)
+
+
+ // append yoga pose image to content output
+contentOutput.appendChild(yogaPoseImage());
+
+ // access description container 
+const descriptionContainer = document.getElementById('description-container')
+// append restaurant description to description container
+descriptionContainer.appendChild(restaurantDescription())
+
+setInterval(flipChartTitle, 2000);
+
+
+
+
+
+// // get nav ul element
+// const ulElement = document.getElementById('nav-ul')
+// // get li elements of nav bar
+// const liElements = ulElement.getElementsByTagName('li');
+// for (let i = 0; i < liElements.length; i++) {
+//     const element = liElements[i];
+//     element.addEventListener('click', (event) => {
+//  // clear the elements in content container
+        
+// // determine which li item was clicked
+// if (event.target.id === 'Lunch') {
+//     ClearContentDom(contentOutput);
+//     contentOutput.appendChild(lunchHeading())
+//     contentOutput.appendChild(smallBitesSelection())
+//     contentOutput.appendChild(houseBurgersSelection())
+//     contentOutput.appendChild(mainPlatesSelection())
+//     // contentOutput.appendChild(optionalOmeletteSelection())
+
+
+// }
+
+// if (event.target.id === 'Breakfast') {
+//     ClearContentDom(contentOutput);
+//     contentOutput.appendChild(breakfastHeading())
+//     contentOutput.appendChild(bakedSelection())
+//     contentOutput.appendChild(breakfastSelection())
+//     contentOutput.appendChild(optionalOmeletteSelection())
+// }
+//     })
+
+// }
+// function ClearContentDom(parentElement) {
+    
+//     // loop through all child elements of content container
+// // for (let i = 0; i < contentOutput.children.length; i++) {
+// //     const childElements = contentOutput.children[i];
+// //     contentOutput.removeChild(childElements)
+    
+
+// while (parentElement.firstChild) {
+//     parentElement.removeChild(parentElement.firstChild)
+// }
+
+
+// }
+
+
+
+
+
---
+++ src/page-load.js
@@ -0,0 +1,102 @@
+import yogaPoseImg from './yoga-pose.png'
+
+function instructorName() {
+    const div = document.createElement('div');
+    const h1 = document.createElement('h1');
+h1.innerHTML = 'Paula Walsh';
+
+div.appendChild(h1)
+
+    return div;
+}
+function pageLoadNav() {
+// create nav container 
+    const navContainer = document.createElement('div');
+navContainer.id = 'nav-container';
+// create ul element 
+const ulElement = document.createElement('ul');
+ulElement.id = 'nav-ul';
+// create array of list items
+const navItems = ['About Me', 'Upcoming Events', 'Contact Me']
+
+// loop through nav items
+for (let i = 0; i < navItems.length; i++) {
+// create li element
+const liElement = document.createElement('li');
+// set id of list items
+liElement.id = navItems[i]
+// set text content of each li element
+liElement.textContent = navItems[i];
+
+// append li elements to ul element
+ulElement.appendChild(liElement);
+
+// liElement.addEventListener('click', (event) => {
+//     alert('hello' + event.target.textContent)
+// })
+}
+// append ul element to nav container
+navContainer.appendChild(ulElement)
+
+return navContainer;
+
+}
+
+
+function yogaPoseImage() {
+    const descriptionContainer = document.createElement('div');
+    const imgContainer = document.createElement('div');
+
+descriptionContainer.id = 'description-container';
+imgContainer.id = 'img-container';
+
+    const img = new Image();
+    img.src = yogaPoseImg;
+    img.alt = 'women holding yoga pose with full moon in background';
+
+imgContainer.style.backgroundImage = 'url(img)'
+
+
+    descriptionContainer.appendChild(imgContainer)
+imgContainer.appendChild(img)
+
+
+    return descriptionContainer;
+}
+function restaurantDescription() {
+    const div = document.createElement('div');
+    div.id = 'text-container';
+const h1 = document.createElement('h1');
+h1.id = 'section-1-title';
+// const p = document.createElement('p');
+
+h1.innerHTML = '';
+// p.innerHTML = 'At our establishment, we take pride in ensuring that all of our dishes are freshly prepared. We hold a strong aversion to the use of pesticides and preservatives. To uphold our commitment to quality, we strive to incorporate local, organic, and free-range ingredients whenever possible, depending on their availability. Listed below are some of our most beloved dishes. We encourage you to visit us and experience our ever-evolving menu firsthand.';
+
+div.appendChild(h1)
+// div.appendChild(p)
+return div;
+}
+
+const flipChartTitles = ['yoga instructor', 'nutritionist', 'spiritual healer'];
+    let currentIndex = 0;
+function flipChartTitle() {
+    
+
+  const flipChartElement = document.getElementById("section-1-title");
+  flipChartElement.textContent = flipChartTitles[currentIndex];
+  
+  currentIndex = (currentIndex + 1) % flipChartTitles.length;
+}
+
+
+
+
+
+export {
+    instructorName,
+     yogaPoseImage,
+      restaurantDescription,
+       pageLoadNav,
+       flipChartTitle, 
+     }; 
